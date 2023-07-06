f5c487fd1d8c8d8f9158b4a66145b7194fa59e65
Ana Grácio <anitagracio@hotmail.com>
2023-07-06 17:12:10 +0100

    Keep code format

--- src/pages/Ocurrences/Index.js
+++ src/pages/Ocurrences/Index.js
@@ -49,63 +49,63 @@ class OccurrencesIndex extends React.Component {
         }
 
         return (
-            <div>
+            <>
                 <Navbar />
                 <div className="w-full flex flex-col items-center py-8">
-                    <section class="container px-4 mx-auto">
-                    <h2 class="text-lg font-medium text-gray-800 dark:text-gray-800">Lista de Ocorrências</h2>
-                        <div class="flex flex-col mt-6">
-                            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                                <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                                    <div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                    <section className="container px-4 mx-auto">
+                    <h2 className="text-lg font-medium text-gray-800 dark:text-gray-800">Lista de Ocorrências</h2>
+                        <div className="flex flex-col mt-6">
+                            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                                    <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
 
-                                        <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                                            <thead class="bg-gray-50 dark:bg-gray-800">
+                                        <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                                            <thead className="bg-gray-50 dark:bg-gray-800">
                                                 <tr>
-                                                    <th scope="col" class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                    <th scope="col" className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                                         <span>Título</span>
                                                     </th>
 
-                                                    <th scope="col" class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                    <th scope="col" className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                                         Estado
                                                     </th>
 
-                                                    <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                    <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                                     </th>
 
-                                                    <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                    <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                                     </th>
 
-                                                    <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                    <th scope="col" className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                                     </th>
 
                                                 </tr>
                                             </thead>
-                                            <tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                                            <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
                                                 {occurrences.map(occurrence => (
                                                 <tr key={occurrence.id}>
-                                                    <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
+                                                    <td className="px-4 py-4 text-sm font-medium whitespace-nowrap">
                                                         <div>
-                                                            <h2 class="font-medium text-gray-800 dark:text-white ">{occurrence.title}</h2>
+                                                            <h2 className="font-medium text-gray-800 dark:text-white ">{occurrence.title}</h2>
                                                         </div>
                                                     </td>
-                                                    <td class="px-12 py-4 text-sm font-medium whitespace-nowrap">
-                                                        <div class="inline px-3 py-1 text-sm font-normal text-gray-500 bg-gray-100 rounded-full dark:text-gray-400 gap-x-2 dark:bg-gray-800">
+                                                    <td className="px-12 py-4 text-sm font-medium whitespace-nowrap">
+                                                        <div className="inline px-3 py-1 text-sm font-normal text-gray-500 bg-gray-100 rounded-full dark:text-gray-400 gap-x-2 dark:bg-gray-800">
                                                             {occurrence.currentState}
                                                         </div>
                                                     </td>
-                                                    <td class="px-4 py-4 text-sm whitespace-nowrap">
-                                                        <div class="flex items-center gap-x-6">
+                                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                                        <div className="flex items-center gap-x-6">
                                                             <div className="text-gray-900 whitespace-no-wrap tracking-wider font-bold rounded-md bg-blue-400 hover:bg-blue-500 px-4 py-2">Detalhes</div>
                                                         </div>
                                                     </td>
-                                                    <td class="px-4 py-4 text-sm whitespace-nowrap">
-                                                        <div class="flex items-center gap-x-6">
+                                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                                        <div className="flex items-center gap-x-6">
                                                             <div className="text-gray-900 whitespace-no-wrap tracking-wider font-bold rounded-md bg-yellow-400 hover:bg-yellow-500 px-4 py-2">Editar</div>
                                                         </div>
                                                     </td>
-                                                    <td class="px-4 py-4 text-sm whitespace-nowrap"> 
-                                                        <div class="flex items-center gap-x-6">
+                                                    <td className="px-4 py-4 text-sm whitespace-nowrap"> 
+                                                        <div className="flex items-center gap-x-6">
                                                             <div className="text-gray-900 whitespace-no-wrap tracking-wider font-bold rounded-md bg-red-400 hover:bg-red-500 px-6 py-2">Apagar</div>
                                                         </div>
                                                     </td>
@@ -121,8 +121,7 @@ class OccurrencesIndex extends React.Component {
                 </div>
                 <br />
                 <Footer />
-            </div>
-            
+            </>
         );
     }
 }

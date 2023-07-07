4dc1e1876ca6583ad8f106a6177608d3e26c3131
Ana Grácio <anitagracio@hotmail.com>
2023-07-07 17:01:44 +0100

    Change button text

--- src/pages/Ocurrences/Edit.js
+++ src/pages/Ocurrences/Edit.js
@@ -188,7 +188,7 @@ class OccurrencesEdit extends React.Component {
                         </div>
 
                         <div className="flex justify-end mt-6">
-                            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Criar</button>
+                            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Editar</button>
                         </div>
                     </form>
                 </section>

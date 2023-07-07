4a861bc6adf93e70a9a47600e4e36b24e0a08c07
Daniel Graça <danielgrascas@gmail.com>
2023-07-07 17:05:00 +0100

    [FIX] Footer responsiveness

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -15,13 +15,11 @@ class Navbar extends React.Component {
                 
                         <p class="text-sm text-gray-600 dark:text-gray-300">© Copyright 2021. All Rights Reserved.</p>
 
-                        <div className="flex flex-row items-center justify-between w-full px-8">
-                            <div className="mt-4 flex flex-col sm:flex-row items-center justify-center text-center gap-8">
+                        <div className="flex flex-col sm:flex-row items-center justify-between w-full px-8">
+                            <div className="mt-4 flex flex-col sm:flex-row items-center justify-end text-center gap-8 w-full">
                                 <p><a href="https://reactjs.org" target="_blank" className="max-w-md mx-auto mt-2 text-gray-400 hover:text-gray-200">React</a></p>
                                 <p><a href="https://reactrouter.com/" target="_blank" className="max-w-md mx-auto mt-2 text-gray-400 hover:text-gray-200">React router</a></p>
                                 <p><a href="https://tailwindcss.com/" target="_blank" className="max-w-md mx-auto mt-2 text-gray-400 hover:text-gray-200">TailwindCSS</a></p>
-                            </div>
-                            <div className="mt-4 flex flex-col sm:flex-row items-center justify-center text-center gap-8">
                                 <p><a href="https://merakiui.com/" target="_blank" className="max-w-md mx-auto mt-2 text-gray-400 hover:text-gray-200">MerakiUI</a></p>
                                 <p><a href="https://sweetalert2.github.io/" target="_blank" className="max-w-md mx-auto mt-2 text-gray-400 hover:text-gray-200">Sweetalert2</a></p>
                             </div>
